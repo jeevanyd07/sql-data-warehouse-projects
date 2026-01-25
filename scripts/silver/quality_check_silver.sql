5c765b0fd71cf9c3a81fd11a782153582bceeec7
JEEVAN Y D <jeevanyd07@gmail.com>
2026-01-26 01:52:24 +0530

    Create quality_check_silver.sql

---
+++ scripts/silver/quality_check_silver.sql
@@ -0,0 +1,18 @@
+/*
+=================================================================================
+Quality Checks
+=================================================================================
+Script Purpose:
+    This script performs various quality checks for data consistency, accuracy,
+    and standardization across the 'silver' schema. It includes checks for:
+    - Null or duplicates primary keys.
+    - Unwanted spaces in string fields.
+    - Data standarization and consistency.
+    - Invalid data ranges and others.
+    - Data consistency between related fields.
+
+Usage Notes:
+    - Run these checks after data loading Layer.
+    - Invessting and resolve any discrepancies found duaring the checks.
+===============================================================================
+*/
