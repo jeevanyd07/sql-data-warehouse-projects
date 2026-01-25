719b7db9872b59911f90c1a83b10b61a7cd7e3dd
JEEVAN Y D <jeevanyd07@gmail.com>
2026-01-26 00:57:39 +0530

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -1,192 +1,188 @@
--- Creating Silver (CRM) Layer Customer Information --
-TRUNCATE TABLE bronz."silver.crm_cust_info";
-INSERT INTO bronz."silver.crm_cust_info"(
-	cst_id,
-	cst_key,
-	cst_firstname,
-	cst_lastname,
-	cst_material_status,
-	cst_gndr,
-	cst_create_dste
-)
-SELECT 
-cst_id,
-cst_key,
-TRIM(cst_firstname),
-TRIM(cst_lastname),
-CASE
-	WHEN cst_material_status = 'M' THEN 'Married'
-	WHEN cst_material_status = 'S' THEN 'Single'
-	ELSE 'N/A'
-END cst_material_status,
-CASE
-	WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
-	WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
-	ELSE 'N/A'
-END cst_gndr,
-cst_create_date
-FROM(
-SELECT 
-* ,
-ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
-FROM bronz."bronze.crm_cust_info")T
-WHERE flag_last = 1;
 
--------------------------------------------------------------------------------------------------------------------------
 
--- Creating Silver (CRM) Layer Product Information --
-SELECT * FROM bronz."silver.crm_prd_info";
-TRUNCATE TABLE bronz."silver.crm_prd_info";
-INSERT INTO bronz."silver.crm_prd_info"(
-	prd_id,
-	cat_id,
-	prd_key,
-	prd_nm,
-	prd_cost,
-	prd_line,
-	prd_start_dt,
-	prd_end_dt
-)
-SELECT  
-prd_id,
-REPLACE(SUBSTRING(prd_key,1, 5),'-','_') AS cat_id,
-SUBSTRING(prd_key, 7,LENGTH(prd_key)) AS prd_key,
-prd_nm,
-COALESCE(prd_cost, 0) AS prd_cost,
-CASE
-	WHEN UPPER(TRIM(prd_line)) = 'M' THEN 'Mountain'
-	WHEN UPPER(TRIM(prd_line)) = 'R' THEN 'Road'
-	WHEN UPPER(TRIM(prd_line)) = 'S' THEN 'Other Sales'
-	WHEN UPPER(TRIM(prd_line)) = 'T' THEN 'Touring'
-	ELSE 'N/a'
-END AS prd_line,
-CAST(prd_start_dt AS DATE) AS prd_start_dt,
-CAST(LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt)-1 AS DATE) AS prd_end_dt 
-FROM bronz."bronze.crm_prd_info";
+CREATE OR REPLACE PROCEDURE bronz.load_silver()
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    ----------------------------------------------------------------
+    -- Silver CRM Customer Info
+    ----------------------------------------------------------------
+    TRUNCATE TABLE bronz."silver.crm_cust_info";
 
------------------------------------------------------------------------------------------------------------------
+    INSERT INTO bronz."silver.crm_cust_info" (
+        cst_id,
+        cst_key,
+        cst_firstname,
+        cst_lastname,
+        cst_material_status,
+        cst_gndr,
+        cst_create_dste
+    )
+    SELECT 
+        cst_id,
+        cst_key,
+        TRIM(cst_firstname),
+        TRIM(cst_lastname),
+        CASE
+            WHEN cst_material_status = 'M' THEN 'Married'
+            WHEN cst_material_status = 'S' THEN 'Single'
+            ELSE 'N/A'
+        END AS cst_material_status,
+        CASE
+            WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+            WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+            ELSE 'N/A'
+        END AS cst_gndr,
+        cst_create_date
+    FROM (
+        SELECT *,
+               ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
+        FROM bronz."bronze.crm_cust_info"
+    ) t
+    WHERE flag_last = 1;
 
--- Creating Silver (CRM) Layer Sales Details
-TRUNCATE TABLE bronz."silver.crm_sales_details";
-INSERT INTO bronz."silver.crm_sales_details"(
-	sls_ord_num,
-	sls_prd_key,
-	sls_cust_id,
-	sls_order_dt,
-	sls_ship_dt,
-	sls_due_dt,
-	sls_sales,
-	sls_quantity,
-	sls_price
-)
-SELECT 
-sls_ord_num,
-sls_prd_key,
-sls_cust_id,
-CASE
-	WHEN sls_order_id = 0 OR LENGTH(CAST(sls_order_id AS VARCHAR)) != 8 THEN NULL
-	ELSE CAST(CAST(sls_order_id AS VARCHAR) AS DATE)
-END AS sls_order_dt,
-CASE
-	WHEN sls_ship_dt = 0 OR LENGTH(CAST(sls_ship_dt AS VARCHAR)) != 8 THEN NULL
-	ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
-END AS sls_order_dt,
-CASE
-	WHEN sls_due_dt = 0 OR LENGTH(CAST(sls_due_dt AS VARCHAR)) != 8 THEN NULL
-	ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
-END AS sls_due_dt,
-CASE
-	WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * sls_price
-	THEN sls_quantity * ABS(sls_price)
-	ELSE sls_sales
-END AS sla_sales,
-sls_quantity,
-CASE 
-	WHEN sls_price IS NULL OR sls_price <= 0 THEN sls_sales / NULLIF(sls_quantity,0)
-	ELSE sls_price
-END AS sls_price
-FROM bronz."bronze.crm_sales_details";
+    ----------------------------------------------------------------
+    -- Silver CRM Product Info
+    ----------------------------------------------------------------
+    TRUNCATE TABLE bronz."silver.crm_prd_info";
 
-----------------------------------------------------------------------------------------------------------------
+    INSERT INTO bronz."silver.crm_prd_info" (
+        prd_id,
+        cat_id,
+        prd_key,
+        prd_nm,
+        prd_cost,
+        prd_line,
+        prd_start_dt,
+        prd_end_dt
+    )
+    SELECT  
+        prd_id,
+        REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id,
+        SUBSTRING(prd_key, 7) AS prd_key,
+        prd_nm,
+        COALESCE(prd_cost, 0),
+        CASE
+            WHEN UPPER(TRIM(prd_line)) = 'M' THEN 'Mountain'
+            WHEN UPPER(TRIM(prd_line)) = 'R' THEN 'Road'
+            WHEN UPPER(TRIM(prd_line)) = 'S' THEN 'Other Sales'
+            WHEN UPPER(TRIM(prd_line)) = 'T' THEN 'Touring'
+            ELSE 'N/A'
+        END AS prd_line,
+        CAST(prd_start_dt AS DATE),
+        CAST(LEAD(prd_start_dt)
+             OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - 1 AS DATE)
+    FROM bronz."bronze.crm_prd_info";
 
--- Creating Silver (ERP) Customer az12 Table
-TRUNCATE TABLE bronz."silver.erp_cust_az12";
-INSERT INTO bronz."silver.erp_cust_az12"(
-	cid,
-	bdate,
-	gen
-)
-SELECT 
-CASE 
-	WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid,4,LENGTH(cid))
-	ELSE cid
-END AS cid,
-CASE
-	WHEN bdate > NOW() THEN NULL
-	ELSE bdate
-END AS bdate,
-CASE 
-	WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
-	WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
-END AS gen
-FROM bronz."bronze.erp_cust_az12";
+    ----------------------------------------------------------------
+    -- Silver CRM Sales Details
+    ----------------------------------------------------------------
+    TRUNCATE TABLE bronz."silver.crm_sales_details";
 
---------------------------------------------------------------------------------------------------------------
+    INSERT INTO bronz."silver.crm_sales_details" (
+        sls_ord_num,
+        sls_prd_key,
+        sls_cust_id,
+        sls_order_dt,
+        sls_ship_dt,
+        sls_due_dt,
+        sls_sales,
+        sls_quantity,
+        sls_price
+    )
+    SELECT 
+        sls_ord_num,
+        sls_prd_key,
+        sls_cust_id,
+        CASE
+            WHEN sls_order_id = 0 OR LENGTH(sls_order_id::TEXT) <> 8 THEN NULL
+            ELSE TO_DATE(sls_order_id::TEXT, 'YYYYMMDD')
+        END,
+        CASE
+            WHEN sls_ship_dt = 0 OR LENGTH(sls_ship_dt::TEXT) <> 8 THEN NULL
+            ELSE TO_DATE(sls_ship_dt::TEXT, 'YYYYMMDD')
+        END,
+        CASE
+            WHEN sls_due_dt = 0 OR LENGTH(sls_due_dt::TEXT) <> 8 THEN NULL
+            ELSE TO_DATE(sls_due_dt::TEXT, 'YYYYMMDD')
+        END,
+        CASE
+            WHEN sls_sales IS NULL
+              OR sls_sales <= 0
+              OR sls_sales <> sls_quantity * sls_price
+            THEN sls_quantity * ABS(sls_price)
+            ELSE sls_sales
+        END,
+        sls_quantity,
+        CASE
+            WHEN sls_price IS NULL OR sls_price <= 0
+            THEN sls_sales / NULLIF(sls_quantity, 0)
+            ELSE sls_price
+        END
+    FROM bronz."bronze.crm_sales_details";
 
--- Creating Silver (ERP) Customer az12 Table
-TRUNCATE TABLE bronz."silver.erp_cust_az12";
-INSERT INTO bronz."silver.erp_cust_az12"(
-	cid,
-	bdate,
-	gen
-)
-SELECT 
-CASE 
-	WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid,4,LENGTH(cid))
-	ELSE cid
-END AS cid,
-CASE
-	WHEN bdate > NOW() THEN NULL
-	ELSE bdate
-END AS bdate,
-CASE 
-	WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
-	WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
-END AS gen
-FROM bronz."bronze.erp_cust_az12";
+    ----------------------------------------------------------------
+    -- Silver ERP Customer AZ12
+    ----------------------------------------------------------------
+    TRUNCATE TABLE bronz."silver.erp_cust_az12";
 
-----------------------------------------------------------------------------------------------------------
+    INSERT INTO bronz."silver.erp_cust_az12" (
+        cid,
+        bdate,
+        gen
+    )
+    SELECT 
+        CASE
+            WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4)
+            ELSE cid
+        END,
+        CASE
+            WHEN bdate > NOW() THEN NULL
+            ELSE bdate
+        END,
+        CASE
+            WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
+            WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
+            ELSE 'N/A'
+        END
+    FROM bronz."bronze.erp_cust_az12";
 
--- Creating Silver (ERP) Location a101 Table
-TRUNCATE TABLE bronz."silver.erp_loc_a101";
-INSERT INTO bronz."silver.erp_loc_a101"(
-	cid,
-	cntry
-)
-SELECT
-REPLACE(cid,'-','')cid,
-CASE
-	WHEN TRIM(cntry) = 'DE' THEN 'Germany'
-	WHEN TRIM(cntry) IN ('US','USA') THEN 'United States'
-	WHEN TRIM(cntry) = ' ' OR cntry IS NULL THEN 'N/A'
-	ELSE TRIM(cntry)
-END AS cntry
-FROM bronz."bronze.erp_loc_a101";
+    ----------------------------------------------------------------
+    -- Silver ERP Location A101
+    ----------------------------------------------------------------
+    TRUNCATE TABLE bronz."silver.erp_loc_a101";
 
---------------------------------------------------------------------------------------
+    INSERT INTO bronz."silver.erp_loc_a101" (
+        cid,
+        cntry
+    )
+    SELECT
+        REPLACE(cid, '-', ''),
+        CASE
+            WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+            WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
+            WHEN cntry IS NULL OR TRIM(cntry) = '' THEN 'N/A'
+            ELSE TRIM(cntry)
+        END
+    FROM bronz."bronze.erp_loc_a101";
 
--- Creating Silver (ERP) Catalouge Table
-TRUNCATE TABLE bronz."silver.erp_px_cat_g1v2";
-INSERT INTO bronz."silver.erp_px_cat_g1v2"(
-	id,
-	cat,
-	subcat,
-	maintenance
-)
-SELECT
-id,
-cat,
-subcat,
-maintenance
-FROM bronz."bronze.erp_px_cat_g1v2";
+    ----------------------------------------------------------------
+    -- Silver ERP Catalogue
+    ----------------------------------------------------------------
+    TRUNCATE TABLE bronz."silver.erp_px_cat_g1v2";
 
+    INSERT INTO bronz."silver.erp_px_cat_g1v2" (
+        id,
+        cat,
+        subcat,
+        maintenance
+    )
+    SELECT
+        id,
+        cat,
+        subcat,
+        maintenance
+    FROM bronz."bronze.erp_px_cat_g1v2";
+
+END;
+$$;
