35e7c065ff1539a7137098863d15d74755841cde
JEEVAN Y D <jeevanyd07@gmail.com>
2026-01-26 21:58:02 +0530

    Update quality_check_gold.sql

--- scripts/gold/quality_check_gold.sql
+++ scripts/gold/quality_check_gold.sql
@@ -1,4 +1,5 @@
-SELECT cst_id, COUNT(*) FROM( -- Find Out Any Duplicates
+-- Find Out Any Duplicates --
+SELECT cst_id, COUNT(*) FROM( 
 SELECT 
 ci.cst_id,
 ci.cst_key,
@@ -17,3 +18,18 @@ LEFT JOIN bronz."silver.erp_loc_a101" la
 ON ci.cst_key = la.cid
 )t GROUP BY cst_id
 HAVING COUNT(*)>1;
+
+-- Data Integration Gender in customer information --
+SELECT DISTINCT 
+ci.cst_gndr,
+ca.gen,
+CASE
+	WHEN ci.cst_gndr != 'N/A' THEN ci.cst_gndr
+	ELSE COALESCE(ca.gen,'N/A')
+END AS new_gen
+FROM bronz."silver.crm_cust_info" ci
+LEFT JOIN bronz."silver.erp_cust_az12" ca
+ON ci.cst_key = ca.cid
+LEFT JOIN bronz."silver.erp_loc_a101" la
+ON ci.cst_key = la.cid
+ORDER BY 1,2;
