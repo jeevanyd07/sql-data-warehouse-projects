bf7b51fad337bb9f08ff392d6ebf998fa2687759
JEEVAN Y D <jeevanyd07@gmail.com>
2026-01-26 23:55:05 +0530

    Update ddl_gold.sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -19,3 +19,24 @@ ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key,
 	ON ci.cst_key = ca.cid
 	LEFT JOIN bronz."silver.erp_loc_a101" la
 ON ci.cst_key = la.cid
+
+-- Creating Gold Layer for Product --
+CREATE VIEW gold.dim_products AS 
+SELECT
+	ROW_NUMBER() OVER (ORDER BY pn.prd_start_dt, pn.prd_key) AS product_key,
+	pn.prd_id AS product_id,
+	pn.prd_key AS product_number,
+	pn.prd_nm AS product_nam,
+	pn.cat_id AS category_id,
+	pc.cat AS category,
+	pc.subcat AS subcategory,
+	pc.maintenance,
+	pn.prd_cost AS cost,
+	pn.prd_line AS product_line,
+	pn.prd_start_dt AS start_date
+FROM bronz."silver.crm_prd_info" pn
+LEFT JOIN bronz."silver.erp_px_cat_g1v2" pc
+ON pn.cat_id = pc.id
+WHERE prd_end_dt IS NULL -- Filter out all histrical date
+
+
