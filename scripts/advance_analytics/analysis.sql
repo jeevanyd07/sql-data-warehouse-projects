f6488e6c500c5e2a77fa92b379df0aeebe15de7b
JEEVAN Y D <jeevanyd07@gmail.com>
2026-02-03 15:44:51 +0530

    Update analysis.sql

--- scripts/advance_analytics/analysis.sql
+++ scripts/advance_analytics/analysis.sql
@@ -1,5 +1,7 @@
-
+-- ===========================================================================================================================
 -- Chaange-Over-Time (TRENDS)
+-- ===========================================================================================================================
+
 SELECT 
 -- TO_CHAR(order_date, 'YYYY MON') AS order_date,
 EXTRACT(YEAR FROM order_date) AS order_year,
@@ -12,3 +14,26 @@ WHERE order_date IS NOT NULL
 GROUP BY order_year, order_month
 ORDER BY order_year, order_month;
 
+
+
+-- =============================================================================================================================
+-- Cumulative Analysis: Calculate the total sales per month and the running total of sales over time
+-- ==============================================================================================================================
+
+SELECT 
+order_year_month,
+total_sales,
+SUM(total_sales) OVER (PARTITION BY order_year ORDER BY order_year_month) AS running_total,
+AVG(avg_price) OVER (ORDER BY order_year_month) AS moving_avarage_price
+FROM -- IF YOU WANT ADD PARTITION BY
+(
+SELECT
+EXTRACT(YEAR FROM order_date) AS order_year,
+TO_CHAR(order_date, 'YYYY-MM') AS order_year_month,
+SUM(sales_amount) AS total_sales,
+AVG(price) AS avg_price
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY order_year,order_year_month
+ORDER BY order_year_month
+) t
