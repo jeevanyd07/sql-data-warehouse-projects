d0c5c0c22401283b3d077288d0d7a615eec6bf65
JEEVAN Y D <jeevanyd07@gmail.com>
2026-02-26 11:31:40 +0530

    Update analysis.sql

--- scripts/advance_analytics/analysis.sql
+++ scripts/advance_analytics/analysis.sql
@@ -77,8 +77,25 @@ ORDER BY product_nam, order_year;
 
 
 
+-- =============================================================================================================================
+-- Part to Whole: Which categories contribute the most to overall sales
+-- ==============================================================================================================================
 
+WITH category_sales AS(
+SELECT
+category,
+SUM(sales_amount) AS total_sales
+FROM gold.fact_sales f LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key
+GROUP BY category)
 
+SELECT
+category,
+total_sales,
+SUM(total_sales) OVER () AS overall_sales,
+CONCAT(ROUND((total_sales / SUM(total_sales) OVER())*100,2),'%') AS percentage_of_total
+FROM category_sales
+ORDER BY total_sales DESC;
 
 
 
