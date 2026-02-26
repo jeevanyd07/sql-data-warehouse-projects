44ecfd842527cb59ba469cb95f65d99bcb9c4e01
JEEVAN Y D <jeevanyd07@gmail.com>
2026-02-26 13:22:16 +0530

    Update analysis.sql

--- scripts/advance_analytics/analysis.sql
+++ scripts/advance_analytics/analysis.sql
@@ -100,12 +100,31 @@ ORDER BY total_sales DESC;
 
 
 -- =============================================================================================================================
--- Data Segmentation: Segment products into cost ranges and count how meny fall into each segment
+-- Data Segmentation: 
 -- ==============================================================================================================================
+--Segment products into cost ranges and count how meny fall into each segment:
+WITH product_segments AS(
+SELECT
+product_key,
+product_nam,
+cost,
+CASE 
+	WHEN cost < 100 THEN 'Below 100'
+	WHEN cost BETWEEN 100 AND 500 THEN '100-500'
+	WHEN cost BETWEEN 500 AND 1000 THEN '500-1000'
+	ELSE 'Above 1000'
+END cost_range
+FROM gold.dim_products)
+
+SELECT
+cost_range,
+COUNT(product_key) AS total_products
+FROM product_segments
+GROUP BY cost_range
+ORDER BY total_products DESC;
 
 
-
-
+-- Group customer into three segments based on their spending behavior:
 
 
 
