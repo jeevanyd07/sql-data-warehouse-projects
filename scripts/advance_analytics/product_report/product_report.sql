974c9fd6608e827ee13bfcd50a862bfb7817827e
JEEVAN Y D <jeevanyd07@gmail.com>
2026-03-04 14:41:35 +0530

    Update product_report.sql

--- scripts/advance_analytics/product_report/product_report.sql
+++ scripts/advance_analytics/product_report/product_report.sql
@@ -1,1 +1,23 @@
+/*
+============================================================================================================================
+Product Report
+============================================================================================================================
+Purpose:
+  - This report consolidates key produtc metrics and behaviors
+
+Highlights:
+  1. Gathers essential fields such as product name, category, subcategory, and cost.
+  2. Segments product by revenue to identify High-Performance, Mid-Range, or Low-Range, or Low-Performers
+  3. Aggregates product-level metrics:
+    - total orders
+    - total sales
+    - total quantity sold
+    - total customers (unique)
+    - lifespan (in months)
+  4. Calculates valuable KPIs:
+    - recency (month since last sale)
+    - avarage order revenue (AOR)
+    - average monthly revenue
+============================================================================================================================
+*/
 
