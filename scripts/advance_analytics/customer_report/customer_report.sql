c9dc01d44517fd5c97386114b2cc68f185848c93
JEEVAN Y D <jeevanyd07@gmail.com>
2026-03-04 14:54:44 +0530

    Update customer_report.sql

--- scripts/advance_analytics/customer_report/customer_report.sql
+++ scripts/advance_analytics/customer_report/customer_report.sql
@@ -1,1 +1,22 @@
+/*
+===============================================================================================================================
+Customer Report
+===============================================================================================================================
+Purpose:
+    - This Report consolidates key customer matrics and behaviours
 
+Highlights:
+    1. Gathers essential fields such as names, ages, nad transcations details.
+    2. Segments customers into categories (VIP, Regular, New) and age gropus.
+    3. Aggregates customer-level metrics:
+        - total orders
+        - total sales
+        - total quantity purchased
+        - total products
+        - lifespan (in months)
+    4. Calcultes valuable KPIs:
+        - recency (month since last order)
+        - average order value
+        - average manthly spend
+================================================================================================================================
+*/
