fc58702f20c1bf0f66b62dc3f1ab2746e65b6844
JEEVAN Y D <jeevanyd07@gmail.com>
2026-03-12 23:43:06 +0530

    Update customer_report.sql

--- scripts/advance_analytics/customer_report/customer_report.sql
+++ scripts/advance_analytics/customer_report/customer_report.sql
@@ -20,3 +20,69 @@ Highlights:
         - average manthly spend
 ================================================================================================================================
 */
+
+WITH base_query AS(
+/*--------------------------------------------------------------------------
+1) Base Query: Retriveves core columns from tables
+---------------------------------------------------------------------------*/
+SELECT
+f.order_number,
+f.product_key,
+f.order_date,
+f.sales_amount,
+f.quantity,
+c.customer_key,
+c.customer_number,
+CONCAT(c.first_name, ' ', c.last_name) as fullname,
+DATE_PART('year', AGE(CURRENT_DATE, c.birthdate)) AS age
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+ON c.customer_key = f.customer_key
+WHERE order_date IS NOT NULL)
+
+,customer_aggragation AS(
+/*--------------------------------------------------------------------
+2) Customer Aggregation: Summarizes key metrics at the customer level
+----------------------------------------------------------------------*/
+SELECT
+customer_key,
+customer_number,
+fullname,
+age,
+COUNT(DISTINCT order_number) AS total_orders,
+SUM(sales_amount) AS total_sales,
+SUM(quantity) AS total_quantity,
+COUNT(DISTINCT product_key) AS total_products,
+MAX(order_date) AS last_order_date,
+DATE_PART('month', AGE(MAX(order_date), MIN(order_date))) AS lifespan
+FROM base_query
+GROUP BY
+	customer_key,
+	customer_number,
+	fullname,
+	age
+)
+SELECT 
+customer_key,
+customer_number,
+fullname,
+age,
+CASE 
+	WHEN age < 20 THEN 'Under 20'
+	WHEN age BETWEEN 20 AND 29 THEN '20-29'
+	WHEN age BETWEEN 30 AND 39 THEN '30-39'
+	WHEN age BETWEEN 40 AND 49 THEN '40-49'
+	ELSE '50 and above'
+END customer_segment,
+CASE 
+	WHEN lifespan > 12 AND total_sales > 5000 THEN 'VIP'
+	WHEN lifespan > 12 AND total_sales <= 5000 THEN 'Regular'
+	ELSE 'New'
+END customer_segment,
+total_orders,
+total_sales,
+total_quantity,
+total_products,
+last_order_date,
+lifespan
+FROM customer_aggragation 
